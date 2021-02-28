e5c28abc517f508bf98c2db8d066103b657ab5ff
Rheznendra Praditya <rheznendra@gmail.com>
2021-03-01 00:41:33 +0800

    Update route documentation.

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,10 @@ use Illuminate\Support\Facades\Route;
 /*
 * ->namespace 		: Lokasi Folder Controller
 *
-* ->prefix			: Path atau target url. Contoh: prefix('umkm') = http://website.com/umkm
+* ->prefix			: Parent path atau parent target url.
+*					  Contoh: prefix('umkm') = http://website.com/umkm/create (Create hanya contoh)
 *					  Contoh lain, di dalam route group prefix ada route group prefix lagi,
-*					  prefix('barang') = http://website.com/umkm/barang
+*					  prefix('barang') = http://website.com/umkm/barang/edit (Edit hanya contoh)
 *
 * ->name			: Alias atau shortcut untuk memanggil route. Contoh: name('admin.index),
 *					  di controller/blade bisa dipanggil dengan route('admin.index'), yang mana
