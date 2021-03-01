887a62fedee2f184827885fa8ee66ddf14cc1643
FarouqAnNajaby <rouqfarouq@gmail.com>
2021-03-01 22:26:05 +0700

    update barang

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -31,7 +31,7 @@
 					<i class="fas fa-boxes"></i><span>Barang</span>
 				</a>
 				<ul class="dropdown-menu">
-					<li><a class="nav-link" href="barang.php">Data Barang</a></li>
+					<li class="{{ request()->routeIs('admin.barang.index') ? ' active' : null }}"><a class=" nav-link" href="{{ route('admin.barang.index') }}">Data Barang</a></li>
 					<li><a class="nav-link" href="history_barang.php">History Barang</a></li>
 				</ul>
 			</li>
