61f761d7221d0734d86d614c2e6fdcf580740ee5
rheznendra <rheznendra@gmail.com>
2021-03-27 01:31:49 +0800

    - fix breadcrumb back & add button

--- resources/views/admin/partials/breadcrumb.blade.php
+++ resources/views/admin/partials/breadcrumb.blade.php
@@ -1,27 +1,21 @@
 <div class="section-header">
 
     @if ($attributes->has('backBtn'))
-
     <div class="section-header-back">
-        <a href="{{ $attributes->get('url') }}" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
+        <a href="{{ $attributes->get('backUrl') }}" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
     </div>
-    <h1>{{ $attributes->get('title') }}</h1>
-
-    @elseif($attributes->has('addBtn'))
+    @endif
 
     <h1>{{ $attributes->get('title') }}</h1>
+
+    @if($attributes->has('addBtn'))
     <div class="section-header-button">
-        <a href="{{ $attributes->get('url') }}" class="btn btn-primary">Tambah {{ $attributes->get('title') }}</a>
+        <a href="{{ $attributes->get('addUrl') }}" class="btn btn-primary">Tambah {{ $attributes->get('title') }}</a>
     </div>
-
-    @else
-
-    <h1>{{ $attributes->get('title') }}</h1>
-
     @endif
 
     <div class="section-header-breadcrumb">
-        @if(request()->routeIs('admin*'))
+        @if(request()->segment(1) == 'admin')
         <div class="breadcrumb-item active"><a href="{{ route('admin.index') }}">Dashboard</a></div>
         @endif
         {{ $breadcrumbItem }}
